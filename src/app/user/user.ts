cd3dd0c0cf720249e902f1e34f5a9b443774c37e
Hosna <h.m.hosna@gmail.com>
2025-08-14 20:34:57 +0330

    adding the tasks component

--- src/app/user/user.ts
+++ src/app/user/user.ts
@@ -9,10 +9,7 @@ export class User {
   @Input({ required: true }) id!: string;
   @Input({ required: true }) avatar!: string;
   @Input({ required: true }) name!: string;
-
-  //@Output() select = new EventEmitter<string>();
-
-  select = output<string>();
+  @Output() select = new EventEmitter<string>();
 
   get imagePath() {
     return 'assets/users/' + this.avatar;
